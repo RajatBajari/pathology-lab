7456a73324eddfad42efbf099d7ed7d0738767ed
Rajat <rajatbajari@gmail.com>
2024-07-23 00:35:10 +0530

    Added root route to handle /

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
 require('dotenv').config();
-=======
-require("dotenv").config()
->>>>>>> fa1f085ca7b0cd957c08bfd4da8c64f76d17eae9
 const cors=require('cors');
 const bodyParser =require("body-parser")
 const PORT = process.env.PORT || 8000; 
@@ -16,11 +12,9 @@ const TestRoutes=require("./Routes/testRoutes.js")
 const PatientRoutes=require('./Routes/patientRoutes.js');
 app.use('/test',TestRoutes);
 app.use('/patient',PatientRoutes);
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
 app.listen(PORT, () => {
-<<<<<<< HEAD
     console.log(`Local host is running on PORT ${PORT}`);
 })
-=======
-    console.log("Local host is running on PORT 8000");
-})
->>>>>>> fa1f085ca7b0cd957c08bfd4da8c64f76d17eae9
